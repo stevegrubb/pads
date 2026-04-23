77ddf10ed5c879f13d5f358f360b1e2f5cd5bb99
Steve Grubb <ausearch.1@gmail.com>
2026-04-22 22:51:42 -0400

    overrun patch

--- src/util.c
+++ src/util.c
@@ -465,7 +465,7 @@ hex2mac(const char *mac)
 {
     static char buf[18];
 
-    sprintf(buf, "%X:%02X:%02X:%02X:%02X:%02X",
+    snprintf(buf, sizeof(buf), "%02X:%02X:%02X:%02X:%02X:%02X",
         mac[0], mac[1], mac[2],
         mac[3], mac[4], mac[5]);
 
